2c6fdf6074a1ed6c3b41b6550bb319757ee38335
Xaival <54257745+Xaival@users.noreply.github.com>
2022-05-08 23:27:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
-setup (
+readme = open("./README.md", "r")
+
+setup(
     name="SecuenciaAlfanumerica",
     packages=['SecuenciaAlfanumerica'], # Este debe ser el mismo que el nombre de la carpeta
     version="2022.05.08",
     description="Convierte de un número o números a un grupo de caracteres y viceversa",
+    long_description=readme.read(),
+    long_description_content_type='text/markdown',
     author="Xaival",
     author_email="xaival.dark@gmail.com",
     url='https://github.com/Xaival/Libreria-Python-Secuenciador-alfanumerico', # Usar la URL del repositorio de github
