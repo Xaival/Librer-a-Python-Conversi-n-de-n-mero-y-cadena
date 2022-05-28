3e77f295f34a3c54124c50cb9cd6710d7d967f75
Xaival <54257745+Xaival@users.noreply.github.com>
2022-05-28 03:06:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme = open("./README.md", "r") # Abrir archivo Readme.md (No detecta acentos)
 
 setup(
     name="SecuenciaAlfanumerica", # Nombre
-    packages=['SecuenciaAlfanumerica'], # Este debe ser el mismo que el nombre de la carpeta
+    packages=['SecuenciaAlfanumerica'], # Nombre de la carpeta
     version="2022.05.08", # Version
     description="Convierte de un número o números a un grupo de caracteres y viceversa", # Descripcion corta
     long_description=readme.read(), # Leer archivo
